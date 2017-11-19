165a5c338dce7295ace60083cc344540dd533226
Juan Luis Perez <tipiwiny@gmail.com>
2017-11-19 01:42:13 +0100

    finished auth2 flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+const keys = require('./config/keys');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log(req)
-  res.send({ hi: 'there'})
-});
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
